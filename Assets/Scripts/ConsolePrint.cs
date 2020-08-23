6720d341f650421fe4184cb86eb2858b97aa9127
Solacx <junyong6@gmail.com>
2020-08-23 21:33:55 +1000

    80% complete.

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Not sure if the instructions mean this should start printing 0 or 1
+        // Increment every frame i guess also includes first frame so setting to start at 1
+        count++;
+        Debug.Log($"{gameObject.name}: {count}");
+    }
+}
