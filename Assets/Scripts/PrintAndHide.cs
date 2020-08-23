484b41effa9bd5428bd8343ded055fce574292e3
Solacx <junyong6@gmail.com>
2020-08-23 21:56:01 +1000

    100% complete.

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -8,11 +8,14 @@ public class PrintAndHide : MonoBehaviour
     public Renderer rend;
 
     private int count;
+    private int randNum;
 
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
+
+        randNum = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -22,5 +25,21 @@ public class PrintAndHide : MonoBehaviour
         // Increment every frame i guess also includes first frame so setting to start at 1
         count++;
         Debug.Log($"{gameObject.name}: {count}");
+
+        // Hide
+        switch (gameObject.tag)
+        {
+            case "Red":
+                if (count == 100) {
+                    gameObject.SetActive(false);
+                }
+                break;
+
+            case "Blue":
+                if (count == randNum) {
+                    rend.enabled = false;
+                }
+                break;
+        }
     }
 }
